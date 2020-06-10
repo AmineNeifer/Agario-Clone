d1f365f2b29871cc3f129e4e40ea31b6e2e5723f
AmineNeifer <amineneifer2000@gmail.com>
2020-06-10 23:18:05 +0100

    commented out the restrictions of the area && added the eating of little cells functionnality

--- scripts/blob.js
+++ scripts/blob.js
@@ -3,80 +3,85 @@ function Blob(x, y, r, colour) {
 	this.r = r;
 	this.update = function () {
 		/* updates the place of the bubble every frame */
-		let vel;                                     // velocity
-		let wallReached = {                          // determines the borders for the bubble
-			right: this.pos.x >= width * 2 - this.r * 0.7,          // right border
-			left: this.pos.x <= -width + this.r * 0.7,              // left border...
-			top: this.pos.y >= height * 2 - this.r * 0.7,
-			bot: this.pos.y <= -height + this.r * 0.7
-		};
-		let widthRestricted = wallReached.right || wallReached.left;
-		let heightRestricted = wallReached.top || wallReached.bot;
-		if (widthRestricted) {
-			vel = createVector(validDir(wallReached.left, wallReached.right, "vertical"), mouseY - height / 2);
-		}
-		else if (heightRestricted) {
-			vel = createVector(mouseX - width / 2, validDir(wallReached.bot, wallReached.top, "horizontal"));
-		}
-		else if (wallReached.right && wallReached.top || wallReacher.right && wallReacher.bot)
-		{
-			
-		}
-		else {
-			vel = createVector(mouseX - width / 2, mouseY - height / 2);
-		};
+		// let vel; // velocity
+		// let wallReached = { // determines the borders for the bubble
+		// 	right: this.pos.x >= width * 2 - this.r * 0.7, // right border
+		// 	left: this.pos.x <= -width + this.r * 0.7, // left border...
+		// 	top: this.pos.y >= height * 2 - this.r * 0.7,
+		// 	bot: this.pos.y <= -height + this.r * 0.7
+		// };
+		// let widthRestricted = wallReached.right || wallReached.left;
+		// let heightRestricted = wallReached.top || wallReached.bot;
+		// if (widthRestricted || heightRestricted) {
+		// 	if (widthRestricted) {
+		// 		vel = createVector(validDir(wallReached.left, wallReached.right, "vertical"), mouseY - height / 2);
+		// 	}
+		// 	if (heightRestricted) {
+		// 		vel = createVector(mouseX - width / 2, validDir(wallReached.bot, wallReached.top, "horizontal"));
+		// 	}
+		// } else {
+		// 	vel = createVector(mouseX - width / 2, mouseY - height / 2);
+		// };
+		vel = createVector(mouseX - width / 2, mouseY - height / 2);
+		
 		vel.setMag(5);
 		this.pos.add(vel);
 	}
 	this.show = function () {
 		/* makes the bubble appear */
 		fill(colour);
-		ellipse(this.pos.x, this.pos.y, r * 2);
+		ellipse(this.pos.x, this.pos.y, this.r * 2);
 	}
 	this.tour = function () {
 		/* makes the bubble stay centered and the background move instead */
 		translate(width / 2 - this.pos.x, height / 2 - this.pos.y);
 	}
-
-}
-validDir = function (restriction1, restriction2, restrictionType) {
-	/**
-	 * validDir - function that checks if the bubble doesn't get out of the it's placep5.BandPass()
-	 *
-	 * @restriction1: border, can be top, bottom, right or left border.
-	 * @restriction2: border, can be top, bottom, right or left border.
-	 * @restrictionType: if vertical means that the bubble reached the max top / bottom.
-	 *
-	 * Return: it returns a value which can determine whether the bubble can keep going the same direction or not.
-	 */
-	if (restrictionType == "vertical") {
-		// reached the right or the left border
-		mouseDir = mouseX;
-		windowDimension = windowWidth;
-		center = width / 2;
-	}
-	else if (restrictionType == "horizontal") {
-		// reached the top or the bottom border
-		mouseDir = mouseY;
-		windowDimension = windowHeight;
-		center = height / 2;
-	}
-	if (restriction1 && mouseDir < windowDimension / 2) {
-		// restriction1 can be left or top border reached
-		// if top border is reached and the mouse is still
-		// in the upper-half of the screen (bubble is moving up)
-		// it returns 0 => no more going up
-		return 0;
-	}
-	else if (restriction2 && mouseDir > windowDimension / 2) {
-		// restriction1 can be right or bottom border reached
-		// if right border is reached and the mouse is still
-		// in the right-half of the screen (bubble is moving to the right)
-		// it returns 0 => no more going to the right
-		return 0;
-	}
-	else {
-		// normal state
-		return mouseDir - center;
+	this.eats = function (other) {
+		let d = p5.Vector.dist(this.pos, other.pos);
+		if (d < this.r + (other.r / 2)) {
+			this.r += other.r;
+			this.show();
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
+	validDir = function (restriction1, restriction2, restrictionType) {
+		/**
+		 * validDir - function that checks if the bubble doesn't get out of the it's placep5.BandPass()
+		 *
+		 * @restriction1: border, can be top, bottom, right or left border.
+		 * @restriction2: border, can be top, bottom, right or left border.
+		 * @restrictionType: if vertical means that the bubble reached the max top / bottom.
+		 *
+		 * Return: it returns a value which can determine whether the bubble can keep going the same direction or not.
+		 */
+		if (restrictionType == "vertical") {
+			// reached the right or the left border
+			mouseDir = mouseX;
+			windowDimension = windowWidth;
+			center = width / 2;
+		} else if (restrictionType == "horizontal") {
+			// reached the top or the bottom border
+			mouseDir = mouseY;
+			windowDimension = windowHeight;
+			center = height / 2;
+		}
+		if (restriction1 && mouseDir < windowDimension / 2) {
+			// restriction1 can be left or top border reached
+			// if top border is reached and the mouse is still
+			// in the upper-half of the screen (bubble is moving up)
+			// it returns 0 => no more going up
+			return 0;
+		} else if (restriction2 && mouseDir > windowDimension / 2) {
+			// restriction1 can be right or bottom border reached
+			// if right border is reached and the mouse is still
+			// in the right-half of the screen (bubble is moving to the right)
+			// it returns 0 => no more going to the right
+			return 0;
+		} else {
+			// normal state
+			return mouseDir - center;
+		}
+	}
--- scripts/engine.js
+++ scripts/engine.js
@@ -1,12 +1,12 @@
 let blob;
 let blobs = [];
+
 function setup() {
 	createCanvas(windowWidth, windowHeight - 5);
-	blob = new Blob (width / 2, height / 2, 40, 'red');
+	blob = new Blob(width / 2, height / 2, 40, 'red');
 
-	for (let i = 0; i < 200; i ++)
-	{
-		blobs[i] = new Blob (random(-width , width * 2), random(-height, height * 2), 10,  "green");
+	for (let i = 0; i < 200; i++) {
+		blobs[i] = new Blob(random(-width, width * 2), random(-height, height * 2), 10, "green");
 	}
 }
 
@@ -14,19 +14,23 @@ function draw() {
 	background(220);
 	blob.tour();
 	// translate(width/2-blob.pos.x, height/2-blob.pos.y);
-	push();
-	fill(0);
-	rect(-width, -height , width * 3, height * 3, 20);
-	pop();
+	// push();
+	// fill(0);
+	// rect(-width, -height, width * 3, height * 3, 20);
+	// pop();
 	blob.show();
 	blob.update();
-	for (let i = 0; i < 200; i++)
-	{
+	for (let i = blobs.length - 1; i >= 0; i--) {
 		blobs[i].show();
+		if (blob.eats(blobs[i])) {
+			blobs.splice(i, 1);
+		}
+
 	}
 	fill(220)
 
 }
+
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight - 5);
 }
